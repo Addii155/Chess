a6692028689ab09094a64c61da7a6eeaf8336d22
Addii155 <aditysgoyal11@gmail.com>
2025-11-16 13:19:20 +0530

    feat: initialize frontend with Vite and React setup
    - Added package.json with dependencies for React, Vite, and TypeScript.
    - Included Vite logo SVG in public directory.
    - Created App.css for styling the application.
    - Developed App.tsx as the main component with state management.
    - Added React logo SVG in assets directory.
    - Established global styles in index.css.
    - Set up main.tsx for rendering the application.
    - Configured TypeScript with tsconfig.app.json and tsconfig.node.json.
    - Created Vite configuration file (vite.config.ts) for project setup.

--- backend/src/Game.ts
+++ backend/src/Game.ts
@@ -6,6 +6,7 @@ export class Game {
     move : string[];
     startTime : Date;
     board : Chess;
+    moveCounter :number ;
     constructor(player1:WebSocket, player2:WebSocket)
     {
         this.player1 = player1;
@@ -13,11 +14,31 @@ export class Game {
         this.startTime = new Date();
         this.move = [];
         this.board = new Chess();
+        this.moveCounter = 0;
+        player1.send(JSON.stringify({type:'game_start', color:'w'}));
+        player2.send(JSON.stringify({type:'game_start', color:'b'}));
 
     }
     makeMove(socket,move: {from:string, to:string})
     {
-    
+        if((this.moveCounter %2 ===0 && socket === this.player1) || (this.moveCounter %2 ===1 && socket === this.player2))
+        {
+            try {
+                const result = this.board.move({from:move.from, to:move.to});
+                if(result)
+                {
+                    this.moveCounter++;
+                    const message = JSON.stringify({type:'move_made', move: result});
+                    this.player1.send(message);
+                    this.player2.send(message);
+                }
+            } catch (error: any) {
+                console.error('Invalid move:', error);
+                socket.send(JSON.stringify({type:'invalid_move', error: error.message}));
+                
+
+            }
+        }
     }
 
 }
